81693fa67c9b0ca6c253871c29d6798f328a95db
EC2 Default User <ec2-user@ip-172-31-24-132.ap-northeast-1.compute.internal>
2023-03-20 12:44:44 +0000

    setting

--- db/migrate/20230109042231_create_player_tags.rb
+++ db/migrate/20230109042231_create_player_tags.rb
@@ -1,8 +1,8 @@
 class CreatePlayerTags < ActiveRecord::Migration[6.1]
   def change
     create_table :player_tags do |t|
-      t.references :player, null: false, foreign_key: true
-      t.references :tag, null: false, foreign_key: true
+      t.integer :player_id, null: false, foreign_key: true
+      t.integer :tag_id, null: false, foreign_key: true
       t.timestamps
     end
   end
