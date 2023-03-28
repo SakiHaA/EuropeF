fbea1acb9140d6b9b903c018e3c97636c05d3823
EC2 Default User <ec2-user@ip-172-31-24-132.ap-northeast-1.compute.internal>
2023-03-28 08:27:44 +0000

    アラートの日本語修正

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -23,7 +23,7 @@ class Public::UsersController < ApplicationController
     @user = current_user
     @user.update(is_deleted: true)
     reset_session
-    redirect_to root_path, notice: '退会しました'
+    redirect_to root_path, notice: '退会しました。'
   end
 
   private
